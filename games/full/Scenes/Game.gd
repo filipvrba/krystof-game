6e5404f693f09b9ad377d40dbb9f3066adea1f26
Filip Vrba <filipvrba@pm.me>
2022-04-04 22:03:08 +0200

    gui score

--- games/full/Scenes/Game.gd
+++ games/full/Scenes/Game.gd
@@ -1,5 +1,8 @@
 extends Spatial
 
+var player1_score = 0
+var player2_score = 0
+
 
 func reset_pitch():
 	get_tree().call_group("game_pieces", "reset")
@@ -9,6 +12,21 @@ func _on_Area_body_entered(body, player_id):
 	print( player_id )
 	get_tree().call_group("game_pieces", "freeze", true)
 	$Timer.start()
+	update_score( player_id )
 
 func _on_Timer_timeout():
 	reset_pitch()
+
+
+func update_score( player_id ):
+	var new_score
+	
+	match player_id:
+		1:
+			player1_score += 1
+			new_score = player1_score
+		2:
+			player2_score += 1
+			new_score = player2_score
+	
+	$GUI.update_score( player_id, new_score )
