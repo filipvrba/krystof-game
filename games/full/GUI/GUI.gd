5553194df629e635e24695bcad2a2942527461ee
Filip Vrba <filipvrba@pm.me>
2022-04-08 18:30:38 +0200

    Add end game

--- games/full/GUI/GUI.gd
+++ games/full/GUI/GUI.gd
@@ -3,3 +3,21 @@ extends CanvasLayer
 func update_score( player, score ):
 	var score_label = get_node("Banner/HBoxContainer/Player%sScore" % player)
 	score_label.text = str( score )
+
+
+func _on_Button_pressed():
+	reset_game_signal()
+
+
+func game_over(player_id):
+	var label = $Popup/VBoxContainer/Label
+	label.text = "Player%s wins!" % str(player_id)
+	$Popup.popup_centered()
+
+
+func _on_Popup_popup_hide():
+	reset_game_signal()
+
+
+func reset_game_signal():
+	get_tree().reload_current_scene()
--- games/full/Scenes/Game.gd
+++ games/full/Scenes/Game.gd
@@ -3,6 +3,8 @@ extends Spatial
 var player1_score = 0
 var player2_score = 0
 
+export var target_score = 3
+
 
 func reset_pitch():
 	get_tree().call_group("game_pieces", "reset")
@@ -30,3 +32,18 @@ func update_score( player_id ):
 			new_score = player2_score
 	
 	$GUI.update_score( player_id, new_score )
+	check_game_over( player_id, new_score )
+
+
+func check_game_over( player, score ):
+	if score == target_score:
+		$Timer.queue_free()
+		$GUI.game_over(player)
+
+
+
+
+
+
+
+
